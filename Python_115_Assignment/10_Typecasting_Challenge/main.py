7d2fc3601a3f1da111c8b77f7ab191352c8e188c
Saquib Hassan <hsakib1243@gmail.com>
2025-04-30 19:47:15 +0600

    answer

---
+++ Python_115_Assignment/10_Typecasting_Challenge/main.py
@@ -0,0 +1,16 @@
+# Given three variables: `a = ‘100’`, `b = 25`, and `c = ‘10.5’`, write a Python program to perform the following operations 
+# and print the results: – Convert `a` to an integer and add it to `b`. – Convert `c` to a float and 
+# subtract it from the result of the first operation. –
+# Convert the final result to a string and concatenate it with the string ” is the answer.”
+
+a = "100"
+b = 25
+c = "10.5"
+
+result1 = int(a) + b
+
+result2 = result1 - float(c)
+
+final_result = str(result2)
+
+print(final_result + " " + "is the answer")
