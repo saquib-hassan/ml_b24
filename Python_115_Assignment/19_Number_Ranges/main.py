48c5d996eba2c8715a94cb9649e784d216d861f8
Saquib Hassan <hsakib1243@gmail.com>
2025-05-01 20:50:55 +0600

    number ranged

---
+++ Python_115_Assignment/19_Number_Ranges/main.py
@@ -0,0 +1,19 @@
+# Write a Python program that takes an integer as input and prints whether the number falls within -
+# the ranges: 0-50, 51-100, 101-150, or above 150.
+
+try:
+    num = int(input("Enter an integer: "))
+
+    if 0 <= num <= 50:
+        print("The number is in the range 0-50.")
+    elif 51 <= num <= 100:
+        print("The number is in the range 51-100.")
+    elif 101 <= num <= 150:
+        print("The number is in the range 101-150.")
+    elif num > 150:
+        print("The number is above 150.")
+    else:
+        print("The number is below 0.")
+
+except ValueError:
+    print("Please enter a valid integer.")
